9bd2be57ab7fc8044ea27ca5c813d74de5533226
catiaraquelabreu <catiamarquesabreu@hotmail.com>
2023-12-12 19:12:02 +0000

    Hotfix

--- src/Components/API/PhotosApi/PhotosApi.jsx
+++ src/Components/API/PhotosApi/PhotosApi.jsx
@@ -1,39 +1,34 @@
-import React, { useEffect, useState } from "react";
-import { createClient } from 'pexels';
 import styles from "./styles.module.scss";
 
-function PhotosApi(props) {
+export default async function fetchImages(props) {
+  const photoID = props.photoID;
+  const APIkey = "ilipp6EwAzKTRaeYRpkClzZl43I9Y9MvqkSUJGyYWV691aQtXTOCnWmoo";
+  const query = "Japan cities";
 
-    const photoID = props.photoID
-
-    const [photos, setPhotos] = useState([]);
-
-    const client = createClient('lipp6EwAzKTRaeYRpkClzZl43I9Y9MvqkSUJGyYWV691aQtXTOCnWmoo');
-
-    const fetchPhotos = async () => {
-    try {
-        const response = await client.photos.search({ query: 'Japan cities' });
-        setPhotos(response.photos);
-    } catch (error) {
-        console.error('Error fetching photos:', error);
+  const imageRequest = await fetch(
+    `https://api.pexels.com/v1/search?query=${query}&per_page=15&page=1`,
+    {
+      headers: {
+        Authorization: APIkey,
+      },
     }
-    };
+  ).then((response) => response.json());
 
-    useEffect(() => {
-        fetchPhotos();
-      }, []);
+  const photos = [...imageRequest.photos].map((image) => ({
+    author: image.photographer,
+    url: image.src.large,
+    id: crypto.randomUUID(),
+  }));
 
-    return (
-        <div>
-            <div className={styles.content}>
-            {photos.slice(0, 1).map((photo, index) => (
-            <div className={styles[{photoID}]} key={photo.id}>
-              <img src={photo.src.original} alt={`Photo ${index}`} />
-            </div>
-            ))}
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <div className={styles.content}>
+        {photos.slice(0, 1).map((photo, index) => (
+          <div className={styles[{ photoID }]} key={photo.id}>
+            <img src={photo.src.original} alt={`Photo ${index}`} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
-
-export default PhotosApi
